365244b8c65bf1370b69de96dab8cfb418796474
Janengethe <janengethew@gmail.com>
2022-04-06 11:54:26 +0300

    catch error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleSignUp() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (results) => {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }, () => {
-      console.log('Signup system offline');
-    },
-  );
+    }).catch(() => console.log('Signup system offline'));
 }
