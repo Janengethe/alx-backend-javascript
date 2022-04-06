7c8a81ce0c370175ae9aaf5b0d460e70925b7a50
Janengethe <janengethew@gmail.com>
2022-04-06 16:01:31 +0300

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+	const usr = await signUpUser(firstName, lastName)
+	.then((result) => {
+		status: 'fulfilled',
+		value: result,
+	});
+	const photo = await uploadPhoto(filename)
+	.catch((error) => ({
+		status: 'rejected',
+		value: error.toString(),
+	}));
+	return Promise.resolve(usr, photo);
+}
