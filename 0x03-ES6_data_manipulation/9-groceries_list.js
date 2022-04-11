9c5f45de9d6d932db2934e8755a2a5357ba1ead2
Janengethe <janengethew@gmail.com>
2022-04-11 15:46:25 +0300

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,20 @@
+export default function groceriesList() {
+  const map1 = new Map();
+  map1.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+  return map1;
+}
+
+// export default function groceriesList() {
+//   const map1 = new Map();
+//   map1.set('Apples', 10);
+//   map1.set('Tomatoes', 10);
+//   map1.set('Pasta', 1);
+//   map1.set('Rice', 1);
+//   map1.set('Banana', 5);
+
+//   return map1;
+// }
