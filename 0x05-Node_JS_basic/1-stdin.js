e1e5bc0868c17c609aa39f8c358d4ec43785805b
Janengethe <janengethew@gmail.com>
2022-05-31 13:39:42 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,10 +4,9 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
-    process.exit();
   }
 });
 
-process.stdin.on('exit', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
