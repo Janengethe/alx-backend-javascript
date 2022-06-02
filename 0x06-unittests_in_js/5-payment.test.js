8c36b18bbf99495c4f07469519644f285cf329b3
Janengethe <janengethew@gmail.com>
2022-06-02 12:48:09 +0300

    task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it('0.', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('1.', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
